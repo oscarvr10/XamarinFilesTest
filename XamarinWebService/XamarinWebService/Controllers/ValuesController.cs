c30a29105ea073e2db0ff10d6f5be3ca9681e6b5
Oscar Vera <vera.reyes.92@outlook.com>
2017-06-08 21:05:56 +0000

    metodo para devolver lsita de archivos

--- XamarinWebService/XamarinWebService/Controllers/ValuesController.cs
+++ XamarinWebService/XamarinWebService/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using XamarinWebService.Models;
 
 namespace XamarinWebService.Controllers
 {
@@ -11,34 +12,22 @@ namespace XamarinWebService.Controllers
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("getFiles")]
+        public JsonResult Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            var listFiles = new List<File>();
+            listFiles.Add(new File()
+            {
+                documentName = "Doc1",
+                documentURL = "http://www.onedrive.com/file1"
+            });
+            listFiles.Add(new File()
+            {
+                documentName = "Doc1",
+                documentURL = "http://www.onedrive.com/file1"
+            });
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Json(listFiles);
         }
     }
 }
---
+++ XamarinWebService/XamarinWebService/Models/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XamarinWebService.Models
+{
+    public class File
+    {
+        public string documentName { get; set; }
+        public string documentURL { get; set; }
+    }
+}
